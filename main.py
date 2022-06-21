a5102f971982ab7b5f79899e9cc64dc51056afb0
Alba Lopes <78361086+profaalbalopes@users.noreply.github.com>
2022-06-21 11:30:50 +0000

    Exemplos com laço de repetição com Flask+Jinja 2

--- main.py
+++ main.py
@@ -10,5 +10,16 @@ def index():
 def cardapio(porcentagem):
   porcentagem = int(porcentagem)
   return render_template('cardapio.html', promocao=True, porcentagem=porcentagem)
+
+
+@app.route('/avaliacoes')
+def avaliacoes():
+  #clientes = ["Arlanda", "Pedro", "Alice", "Bia", "Yanca", "Carlos", "Jonathan"]
+  clientes = [ {"nome": "Arlanda", "nota": 1},
+              {"nome": "Pedro", "nota": 3},
+              {"nome": "Alice", "nota": 5},
+              {"nome": "Bia", "nota": 4},
+             ]
+  return render_template('avaliacoes.html', clientes=clientes)
   
 app.run(host='0.0.0.0', port=81)
